32fbc05595d8ec0c69d5fac69c7c01071913d5bc
Aidan Margo <aidanmargo@gmail.com>
2021-09-22 09:42:53 -0500

    commit tree 13f228d8e5d7b3c93ffb4ccaa6ce913038efd396

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const findMatching = (names, string) => {
+
+  const driverList = names.filter( (name) => {
+    if (name.toLowerCase() == string.toLowerCase()){
+      return name
+    } else {
+      return undefined
+    }
+  })
+
+  return driverList;
+}
+
+const fuzzyMatch = (names, string) => {
+  const driverList = names.filter( (name) => {
+    if(name[0] === string[0]) {
+      return name
+    } else {
+      return undefined
+    }
+  })
+  return driverList
+}
+
+const matchName = (drivers, string) => {
+  const driverList = drivers.filter( (driver) => {
+    if(driver.name === string) {
+      return driver
+    } else {
+      return undefined
+    }
+  })
+  return driverList
+}
